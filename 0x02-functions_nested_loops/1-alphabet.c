38f57a5aba979a874a98dbfebfce1b1fbe0862a3
ajocksheila <ajocksheilah@gmail.com>
2022-07-13 03:04:13 -0700

    lower alphabet numbers

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *  * print_alphabet - prints lowercase alphabet
+ *
+ *   * Description: printing alphabets in lowercase
+ *
+ *    *Return: void
+ */
+void print_alphabet(void)
+{
+char c = 'a';
+while (c <= 'z')
+{
+_putchar(c);
+c++;
+}
+_putchar('\n');
+}
